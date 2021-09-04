fde4906995c522c6b2eeabb64b7a1e17556e202e
ariobramantyo <i.ario.bramantyo@gmail.com>
2021-09-04 14:44:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:quran/view/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quran',
+      home: HomePage(),
+    );
+  }
+}
